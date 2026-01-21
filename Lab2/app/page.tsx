0ba4a7c97b909192f01920354972bd2dac916612
Justin R <Justin.rance@outlook.com>
2026-01-21 15:25:52 -0700

    Revert "Initial commit"
    This reverts commit 077547a0e7b2c16094404486651a6d2c03ccf48b.

---
+++ Lab2/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <main>
+<div><h1> Welcome to CPRG 306: Web Development 2 - Assignments</h1></div>
+<div>
+  <a href="/week2/assignment1"></a>
+  <h2><a href="/week2/">Week 2</a></h2>
+  </div>
+</main>
+  );
+}
---
+++ Lab2/app/week2/student-info.tsx
@@ -0,0 +1,9 @@
+import link from "next/link";
+export default function StudentInfo(){
+return (
+<div>
+    <h1> Name: Justin Rance</h1>
+        <h2><a href="https://github.com/Justinbnr?tab=repositories" style={{color: 'blue'}} >GitHub</a></h2>
+</div>
+);
+}
